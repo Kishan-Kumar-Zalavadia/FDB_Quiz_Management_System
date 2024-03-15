61fcc0ca405a731ac83fbdd753d7a68a9786b330
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2024-03-15 18:02:04 -0500

    NEW: Submit the quiz - display the selected option

--- frontend/src/app/components/take-quiz/take-quiz.component.ts
+++ frontend/src/app/components/take-quiz/take-quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Quiz } from 'src/app/models/quizModel/quiz';
 import { QuizService } from 'src/app/services/quizService/quiz.service';
@@ -10,6 +11,9 @@ import { QuizService } from 'src/app/services/quizService/quiz.service';
 })
 export class TakeQuizComponent implements OnInit {
   quiz = new Quiz();
+  formBuilder: any;
+  quizForm: any;
+  selectedOptions: { [questionId: number]: number } = {};
 
   constructor(
     private route: ActivatedRoute,
@@ -18,11 +22,15 @@ export class TakeQuizComponent implements OnInit {
 
   ngOnInit(): void {
     this.getQuiz();
-    console.log("From Quiz:"+JSON.stringify(this.quiz));
+    console.log('From Quiz:' + JSON.stringify(this.quiz));
   }
 
-  getQuiz(){
+  getQuiz() {
     this.quiz = this.quizService.getQuiz();
   }
 
+  submitQuiz(): void {
+    console.log('Selected Options:', this.selectedOptions);
+    // Now you can send the selected options to your backend or perform any other actions
+  }
 }
